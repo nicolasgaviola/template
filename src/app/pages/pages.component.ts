9cc9533391cffc77bc46f46e4b0def984b2c6010
Nicolás Gaviola <nicolasgaviola@gmail.com>
2019-07-04 09:25:54 -0300

    Se cambian los archivos de configuracion Se realizan cambios en varios archivos para el funcionamiento del mismo

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
+  anio: number = new Date().getFullYear();
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsService {
 
+  ajustes: Ajustes = {
+    temaUrl: 'assets/css/colors/default.css',
+    tema: 'default'
+  };
+
   constructor() { }
+}
+
+interface Ajustes {
+  temaUrl: string;
+  tema: string;
 }
